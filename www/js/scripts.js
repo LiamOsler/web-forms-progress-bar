61e3630b294a09700c20236420efdb12d3d9e7fd
osler <osler@cs.dal.ca>
2021-05-18 20:37:46 -0300

    Created script to toggle input fields sequentially

---
+++ www/js/scripts.js
@@ -0,0 +1,25 @@
+function createAccount1(){
+    document.getElementById("login-1").style.display = "none";
+    document.getElementById("login-2").style.display = "block";
+}
+
+function createAccount2(){
+    document.getElementById("login-2").style.display = "none";
+    document.getElementById("login-3").style.display = "block";
+}
+
+function createAccount3(){
+    document.getElementById("login-3").style.display = "none";
+    document.getElementById("login-4").style.display = "block";
+}
+
+function createAccount4(){
+    document.getElementById("login-4").style.display = "none";
+    document.getElementById("login-5").style.display = "block";
+}
+
+
+function createAccount5(){
+    document.getElementById("login-5").style.display = "none";
+    document.getElementById("login-6").style.display = "block";
+}
