eb570a6f65369e6901712ef00a359218fe5294c1
osler <osler@cs.dal.ca>
2021-05-19 02:25:59 -0300

    Extra script animation calls

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,6 +1,8 @@
 var curProgress = 0;
 
+var x = 0;
 function createAccount1(){
+    x=0;
     curProgress = 1;
     document.getElementById("login-1").style.display = "none";
     document.getElementById("login-2").style.display = "block";
@@ -44,26 +46,102 @@ function createAccount7(){
 
 function createAccount8(){
     curProgress = 8;
-    document.getElementById("login-9").style.display = "none";
-    document.getElementById("login-10").style.display = "block";
 }
 
 
 
 var c = document.getElementById('progressCanvas')
 var ctx = c.getContext("2d");
+var width = c.width;
+var height= c.height;
 
-ctx.fillStyle = "#f73378";
-var x = 0;
+
+ctx.fillStyle = "#00695f";
 setInterval(function(){ 
-
-
-    ctx.clearRect(0,0,200,200);
-    ctx.beginPath();
-
-
-    ctx.rect(0, 0, x, 50);
-    ctx.fill();
+    ctx.clearRect(0,0, width, height);
     
+    if(curProgress==1){
+        if(x>width/(8)){
+            x = width/(8);
+        }
+        ctx.beginPath();
+        ctx.rect(0, 0, x, 200);
+        ctx.fill();
+    }
+
+    if(curProgress==2){
+        if(x>(width/(8))*2){
+            x = width/(8)*2;
+        }
+        ctx.beginPath();
+        ctx.rect(0, 0, x, 200);
+        ctx.fill();
+    }
+
+    if(curProgress==3){
+        if(x>(width/(8))*3){
+            x = width/(8)*3;
+        }
+        ctx.beginPath();
+        ctx.rect(0, 0, x, 200);
+        ctx.fill();
+    }
+
+    
+    if(curProgress==4){
+        if(x>(width/(8))*4){
+            x = width/(8)*4;
+        }
+        ctx.beginPath();
+        ctx.rect(0, 0, x, 200);
+        ctx.fill();
+    }
+
+    if(curProgress==5){
+        if(x>(width/(8))*5){
+            x = width/(8)*5;
+        }
+        ctx.beginPath();
+        ctx.rect(0, 0, x, 200);
+        ctx.fill();
+    }
+
+    if(curProgress==6){
+        if(x>(width/(8))*6){
+            x = width/(8)*6;
+        }
+        ctx.beginPath();
+        ctx.rect(0, 0, x, 200);
+        ctx.fill();
+    }
+
+    if(curProgress==7){
+        if(x>(width/(8))*7){
+            x = width/(8)*7;
+        }
+        ctx.beginPath();
+        ctx.rect(0, 0, x, 200);
+        ctx.fill();
+    }
+    
+    if(curProgress==7){
+        if(x>(width/(8))*7){
+            x = width/(8)*7;
+        }
+        ctx.beginPath();
+        ctx.rect(0, 0, x, 200);
+        ctx.fill();
+    }
+    
+    if(curProgress>7){
+        if(x>width){
+            x = width;
+        }
+        ctx.beginPath();
+        ctx.rect(0, 0, x, 200);
+        ctx.fill();
+    }
+
+
     x++;
 }, 25);
