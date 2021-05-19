ce198376759370283fc929211f5ede20f5f50496
osler <osler@cs.dal.ca>
2021-05-19 00:17:30 -0300

    Styled input forms, added placeholder text

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -18,8 +18,22 @@ function createAccount4(){
     document.getElementById("login-5").style.display = "block";
 }
 
-
 function createAccount5(){
     document.getElementById("login-5").style.display = "none";
     document.getElementById("login-6").style.display = "block";
+}
+
+function createAccount6(){
+    document.getElementById("login-6").style.display = "none";
+    document.getElementById("login-7").style.display = "block";
+}
+
+function createAccount7(){
+    document.getElementById("login-7").style.display = "none";
+    document.getElementById("login-8").style.display = "block";
+}
+
+function createAccount8(){
+    document.getElementById("login-9").style.display = "none";
+    document.getElementById("login-10").style.display = "block";
 }
